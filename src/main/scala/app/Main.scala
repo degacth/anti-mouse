f434d58e5ea3ac474151d9883c0dfb5ee8fff613
Alexander Degtarov <aleksandr.degtyarev@nexign.com>
2026-01-23 12:44:36 +0300

    add splited keys stream

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -6,7 +6,7 @@ import app.layer.activator.{Activator, GlobalKeyListener}
 import app.layer.window.Window
 
 object Main extends ZIOAppDefault:
-  override def run: Task[Any] = application
+  override def run: Task[Any] = application.catchAllCause(debug(_))
 
   private def application: Task[Any] =
     (for
@@ -17,6 +17,8 @@ object Main extends ZIOAppDefault:
             case Activator.Status.Activated => window.activate
             case Activator.Status.Deactivated => window.deactivate
           .fork
+      _ <- window.keyPress.debug.runDrain.fork
+      _ <- window.keyRelease.debug.runDrain.fork
 
       _ <- Console.readLine("ENTER to stop")
       _ <- f.interrupt
--- src/main/scala/app/layer/window/Window.scala
+++ src/main/scala/app/layer/window/Window.scala
@@ -2,13 +2,19 @@ package app.layer.window
 
 import zio.*
 import ZIO.*
+import zio.stream.{UStream, ZStream}
+
 import java.awt.*
+import java.awt.event.{KeyAdapter, KeyEvent}
 import javax.swing.*
 
 object Window:
   trait Service:
     def activate: UIO[Unit]
     def deactivate: UIO[Unit]
+    def keyPress: ZStream[Any, Throwable, KeyEvent]
+    def keyRelease: ZStream[Any, Throwable, KeyEvent]
+    def keys: ZStream[Any, Throwable, (Int, Promise[Nothing, Unit])]
 
   def live: ZLayer[Any, Throwable, Service] = ZLayer.scoped:
     for
@@ -23,10 +29,40 @@ object Window:
         setUndecorated(true)
         setAlwaysOnTop(true)
         val size = 8
-        setBounds(new Rectangle(size, size, size, size))
+        setBounds(Rectangle(size, size, size, size))
         getContentPane.setBackground(Color.MAGENTA)
         setVisible(false)
+
+      p <- Promise.make[Nothing, Unit]
+
+      _ = ZStream.asyncScoped[Any, Throwable, KeyEvent] { cb =>
+          attempt(frame.addKeyListener(new KeyAdapter {
+            override def keyPressed(e: KeyEvent): Unit =
+              cb(succeed(Chunk.single(e)))
+            override def keyReleased(e: KeyEvent): Unit =
+              cb(succeed(Chunk.single(e)))
+          }))
+        }
+        .groupByKey(_.getKeyCode)((k, s) => s.mapAccumZIO(Option.empty[Promise[Nothing, Unit]]) {
+          case (Some(p), e) if e.getID == KeyEvent.KEY_RELEASED => p.succeed(()).map(_ => (None, None))
+          case (None, e) if e.getID == KeyEvent.KEY_PRESSED => Promise.make[Nothing, Unit].map(p => (Some(p), Some(p)))
+          case (s, _) => succeed((s, None))
+        })
+        .collect:
+          case Some(p) => p
+
+      (presses, releases) <- ZStream.asyncScoped[Any, Throwable, KeyEvent] { cb =>
+          attempt(frame.addKeyListener(new KeyAdapter {
+            override def keyPressed(e: KeyEvent): Unit = cb(succeed(Chunk.single(e)))
+            override def keyReleased(e: KeyEvent): Unit = cb(succeed(Chunk.single(e)))
+          })) *> debug("created key listener")
+        }
+        .changesWith((e1, e2) => e1.getKeyCode == e2.getKeyCode && e1.getID == e2.getID)
+        .partition(_.getID == KeyEvent.KEY_PRESSED)
 
     yield new Service:
       override def activate: UIO[Unit] = succeed(frame.setVisible(true))
       override def deactivate: UIO[Unit] = succeed(frame.setVisible(false))
+      override def keyPress: ZStream[Any, Throwable, KeyEvent] = presses
+      override def keyRelease: ZStream[Any, Throwable, KeyEvent] = releases
+      override def keys: ZStream[Any, Throwable, (RuntimeFlags, Promise[Nothing, Unit])] = ???
